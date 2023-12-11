e77fb1df7ccc7a94e4e1621326fcccae60e61c0c
Ning Gong <ning.gong@bkfs.com>
2023-12-11 18:07:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ app.get('/', async (req, res) => {
     await save(bulletins);
     const result = await getAll();
     console.log(`result: ${JSON.stringify(result, null, 2)}`)
-    const target = _.find(result, { key: 'January2024' });
+    const target = _.find(result, { key: 'Febuary2024' });
     // const target = _.find(result, { key: 'ComingSoon' });
 
     if (target) {
-        await send(`January 2024 is available ${JSON.stringify(target, null, 2)}`);
+        await send(`Febuary 2024 is available ${JSON.stringify(target, null, 2)}`);
     }
     res.send(result)
 });
